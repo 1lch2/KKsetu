c30fe66f3385efac7e20a24b074e6e8c02589575
1lch2 <1lch2@163.com>
2026-04-15 17:24:06 +0800

    fix: update tab and input style

--- src/components/TabBar/TabBar.tsx
+++ src/components/TabBar/TabBar.tsx
@@ -1,20 +1,14 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import '../../styles/TabBar.css';
+import './TabBar.css';
 
 const TabBar: React.FC = () => {
   return (
     <div className='tab-bar'>
-      <NavLink
-        to='/'
-        className={({ isActive }) => `tab-button ${isActive ? 'active' : ''}`}
-      >
+      <NavLink to='/' className={({ isActive }) => `tab-button ${isActive ? 'active' : ''}`}>
         提取小红书原图
       </NavLink>
-      <NavLink
-        to='/extract'
-        className={({ isActive }) => `tab-button ${isActive ? 'active' : ''}`}
-      >
+      <NavLink to='/extract' className={({ isActive }) => `tab-button ${isActive ? 'active' : ''}`}>
         提取TXT
       </NavLink>
     </div>
